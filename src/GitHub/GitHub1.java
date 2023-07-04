7b2c0954a9d84fbd1851bc1b7d542b433782d546
Ali Sancaktar <asancaktar01@gmail.com>
2023-07-04 20:49:01 +0300

    new day new game

--- src/GitHub/GitHub1.java
+++ src/GitHub/GitHub1.java
@@ -4,4 +4,12 @@ public class GitHub1 {
     public static void main(String[] args) {
         System.out.println("Sa beyler turk war mi");
     }
+
+    public static String reverse(String str) {
+        String rev = "";
+        for (int i = 0; i < str.length(); i++) {
+            rev = rev + str.charAt(i);
+        }
+        return rev;
+    }
 }
